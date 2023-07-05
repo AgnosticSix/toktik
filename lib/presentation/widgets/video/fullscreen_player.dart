695686f48508996485c1ec9aed6c1bd62e943bdd
AgnosticSix <mateo_godfather@hotmail.com>
2023-07-05 11:53:13 -0600

    readMe updated

--- lib/presentation/widgets/video/fullscreen_player.dart
+++ lib/presentation/widgets/video/fullscreen_player.dart
@@ -40,7 +40,7 @@ class _FullScreenPlayerState extends State<FullScreenPlayer> {
     return FutureBuilder(
       future: controller.initialize(),
       builder: (context, snapshot) {
-        if (snapshot.connectionState != ConnectionState.done){
+        if (snapshot.connectionState != ConnectionState.done) {
           return const Center(
             child: CircularProgressIndicator(
               strokeWidth: 2,
@@ -49,32 +49,35 @@ class _FullScreenPlayerState extends State<FullScreenPlayer> {
         }
         return GestureDetector(
           onTap: () {
-            if(controller.value.isPlaying){
+            if (controller.value.isPlaying) {
               controller.pause();
             } else {
               controller.play();
             }
           },
           onLongPress: () {
-            if(controller.value.isPlaying) controller.pause();
+            if (controller.value.isPlaying) controller.pause();
           },
           onLongPressEnd: (details) {
             controller.play();
           },
           child: AspectRatio(
-            aspectRatio: controller.value.aspectRatio,
-            child: Stack(
-              children: [
-                VideoPlayer(controller),
-                VideoBackground(stops: [0.8, 1.0],),
-                Positioned(
-                  bottom: 50,
-                  left: 20,
-                  child: _VideoCaption(caption: widget.caption,),
-                )
-              ],
-            )
-          ),
+              aspectRatio: controller.value.aspectRatio,
+              child: Stack(
+                children: [
+                  VideoPlayer(controller),
+                  VideoBackground(
+                    stops: [0.8, 1.0],
+                  ),
+                  Positioned(
+                    bottom: 50,
+                    left: 20,
+                    child: _VideoCaption(
+                      caption: widget.caption,
+                    ),
+                  )
+                ],
+              )),
         );
       },
     );
@@ -83,7 +86,10 @@ class _FullScreenPlayerState extends State<FullScreenPlayer> {
 
 class _VideoCaption extends StatelessWidget {
   final String caption;
-  const _VideoCaption({super.key, required this.caption,});
+  const _VideoCaption({
+    super.key,
+    required this.caption,
+  });
 
   @override
   Widget build(BuildContext context) {
@@ -91,8 +97,11 @@ class _VideoCaption extends StatelessWidget {
     final titleStyle = Theme.of(context).textTheme.titleLarge;
 
     return SizedBox(
-      width: size.width * 0.6,
-      child: Text(caption, maxLines: 2, style: titleStyle,)
-    );
+        width: size.width * 0.6,
+        child: Text(
+          caption,
+          maxLines: 2,
+          style: titleStyle,
+        ));
   }
 }
